75daec3bfb402fffa950e0a19d9387ef8eeb34fc
MiracleChibuike <miraclechibuike27@gmail.com>
2025-06-13 11:45:11 +0100

    Corrections and checks

--- dist/about.js
+++ dist/about.js
@@ -46,14 +46,20 @@
 //     });
 // });
 
-document.querySelectorAll('.nav-link[data-section]').forEach(link => {
-  link.addEventListener('click', () => {
-    const section = link.getAttribute('data-section');
+document.querySelectorAll(".nav-link[data-section]").forEach((link) => {
+  link.addEventListener("click", () => {
+    const section = link.getAttribute("data-section");
 
-    if (section === 'home') {
-      window.location.href = 'index.html'; // Navigate to index.html
-    } else {
-      window.location.href = section + '.html'; // Navigate to section.html
+    if (section === "home") {
+      window.location.href = "index.html"; // Navigate to index.html
+    } else if (section === "services") {
+      window.location.href = "Services.html"; // Navigate to services.html
+    } else if (section === "about") {
+      window.location.href = "about.html";
+    } else if (section === "contact") {
+      link.style.color = "rgba(92, 32, 32, 1)";
+      window.location.href = "contact.html"; // Navigate to contact.html
     }
   });
 });
+
